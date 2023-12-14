bd53a698cf33e75d43cbd9352acea4fdd7ed47da
Danivdmijl <32570@ma-web.nl>
2023-12-14 09:38:03 +0100

    les 5-1

--- m10prog-larvel/database/migrations/2023_12_07_154914_create_projects_table.php
+++ m10prog-larvel/database/migrations/2023_12_07_154914_create_projects_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->boolean('active')->default(false);
             $table->timestamps();
+            $table->string('image')->nullable();
+
         });
     }
 
